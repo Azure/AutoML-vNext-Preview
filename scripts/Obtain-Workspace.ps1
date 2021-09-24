fada5e14256b85fe58b4ca5e4532252e41854c6d
Richard Edgar <riedgar@microsoft.com>
2021-09-24 11:50:19 -0400

    Figuring some things out, but workspace list still problematic
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Obtain-Workspace.ps1
+++ scripts/Obtain-Workspace.ps1
@@ -1,13 +1,24 @@
 $baseName="amlisdkv2"
 $location="WestUS"
 $createdTag="createdAt"
+$ownerTeamTagKey="owningTeam"
+$ownerTeamTagValue="AML Intelligence"
+$purposeTagKey="workspacePurpose"
+$purposeTagValue="Automated Tests for DPv2"
 $workspaceYAML="workspace.yaml"
 
-function Get-ExistingWorkspaces
+function Get-RecentWorkspaces(
+    [int]$min_epoch
+)
 {
+    Write-Host "Minimum Epoch: $min_epoch"
     # Would be nice to do this server-side
     $all_workspaces = az ml workspace list | ConvertFrom-Json
-    return Where-Object {$_.friendly_name -contains $baseName}
+
+
+    $filtered_workspaces = $all_workspaces | Where-Object {$_.name.contains($baseName) -and $_.tags.$createdTag -gt $min_epoch}
+
+    return $filtered_workspaces
 }
 
 
@@ -27,8 +38,11 @@ function Create-EpochWorkspace(
     $ws_name = "$basename$epoch_secs"
 
     $ws_data = @{}
-    $ws_data['tags'] = @{ $createdTag="$epoch_secs"}
     $ws_data['name']=$ws_name
+    $ws_data['tags'] = @{}
+    $ws_data['tags'][$createdTag]="$epoch_secs"
+    $ws_data['tags'][$ownerTeamTagKey]=$ownerTeamTagValue
+    $ws_data['tags'][$purposeTagKey]=$purposeTagValue
 
     ConvertTo-Yaml $ws_data | Out-File -FilePath $workspaceYAML -Encoding ascii
 
@@ -37,8 +51,12 @@ function Create-EpochWorkspace(
     az configure --defaults group=$rg_name workspace=$ws_name
 }
 
+# Install-Module powershell-yaml -Scope CurrentUser
+
 $epoch_secs = Get-EpochSecs
 
-Create-EpochWorkspace -epoch_secs $epoch_secs
+# Create-EpochWorkspace -epoch_secs $epoch_secs
 
+$ws_list = Get-RecentWorkspaces($epoch_secs-24*3600)
+Write-Host $ws_list
 
