c8fefd933a0a6588baf14296a23dc7cbfd59b88a
Richard Edgar <riedgar@microsoft.com>
2021-09-24 08:49:39 -0400

    Try running PS script
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ scripts/Install-SDKv2.ps1
@@ -0,0 +1,15 @@
+# Instructions from:
+# https://docs.microsoft.com/en-us/azure/machine-learning/how-to-configure-cli
+
+# Remove old extensions
+az extension remove -n azure-cli-ml
+az extension remove -n ml
+
+# Add the new one
+az extension add -n ml -y
+
+# Check the commands
+az ml -h
+
+# Upgrade to latest version
+az extension update -n ml
