9c22a352095b1393196cce70939f1c717d3607f6
Richard Edgar <riedgar@microsoft.com>
2021-09-24 10:47:33 -0400

    Working things through
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Obtain-Workspace.ps1
+++ scripts/Obtain-Workspace.ps1
@@ -1,4 +1,7 @@
 $baseName="amlisdkv2"
+$location="WestUS"
+$createdTag="createdAt"
+$workspaceYAML="workspace.yaml"
 
 function Get-ExistingWorkspaces
 {
@@ -7,4 +10,35 @@ function Get-ExistingWorkspaces
     return Where-Object {$_.friendly_name -contains $baseName}
 }
 
-Write-Host Get-ExistingWorkspaces()
+
+function Get-EpochSecs
+{
+    # Get time to nearest second
+    $epoch_time =  Get-Date (Get-Date).ToUniversalTime() -UFormat %s
+    $epoch_secs = [Math]::Truncate($epoch_time)
+    return $epoch_secs
+}
+
+function Create-EpochWorkspace(
+    [int]$epoch_secs
+)
+{
+    $rg_name = "$basename-rg-$epoch_secs"
+    $ws_name = "$basename$epoch_secs"
+
+    $ws_data = @{}
+    $ws_data['tags'] = @{ $createdTag="$epoch_secs"}
+    $ws_data['name']=$ws_name
+
+    ConvertTo-Yaml $ws_data | Out-File -FilePath $workspaceYAML -Encoding ascii
+
+    az group create --location $location --name $rg_name --tags $createdTag=$epoch_secs
+    az ml workspace create --resource-group $rg_name --file $workspaceYAML
+    az configure --defaults group=$rg_name workspace=$ws_name
+}
+
+$epoch_secs = Get-EpochSecs
+
+Create-EpochWorkspace -epoch_secs $epoch_secs
+
+
