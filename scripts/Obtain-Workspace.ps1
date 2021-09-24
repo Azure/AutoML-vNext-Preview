119b943cfe11bf833a194685ed6e966957b967d2
Richard Edgar <riedgar@microsoft.com>
2021-09-24 09:34:30 -0400

    Switching to powershell
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Obtain-Workspace.ps1
+++ scripts/Obtain-Workspace.ps1
@@ -1,2 +1,10 @@
-pip install azure-mgmt-resource>=18.0.0 azure-identity>=1.5.0
-python ./scripts/obtain_workspace.py
+$baseName="amlisdkv2"
+
+function Get-ExistingWorkspaces
+{
+    # Would be nice to do this server-side
+    $all_workspaces = az ml workspace list | ConvertFrom-Json
+    return Where-Object {$_.friendly_name -contains $baseName}
+}
+
+Write-Host Get-ExistingWorkspaces()
