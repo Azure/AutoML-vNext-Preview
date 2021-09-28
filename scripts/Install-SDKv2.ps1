7cf551faff8e139c50dee1c28bdbd160265c0438
Richard Edgar <riedgar@microsoft.com>
2021-09-28 11:01:56 -0400

    Need and env variable
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Install-SDKv2.ps1
+++ scripts/Install-SDKv2.ps1
@@ -11,5 +11,8 @@ az extension add -n ml -y
 # Upgrade to latest version
 az extension update -n ml
 
+# Enable all the commands
+$Env:AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED=$true
+
 # Check the commands
 az ml -h
