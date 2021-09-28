0e6ca9d2824b3197fbea7e35e26c0c809077c707
Richard Edgar <riedgar@microsoft.com>
2021-09-28 10:00:26 -0400

    Banging head against brick wall
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ scripts/Register-AzureML.ps1
@@ -0,0 +1,55 @@
+param (
+        [string]$path_to_registration_json
+)
+
+function Read-JsonConfig(
+    [string]$json_filepath
+)
+{
+    Write-Host "Reading $json_filepath"
+    $config = Get-Content $json_filepath | ConvertFrom-Json
+    return $config
+}
+
+function Replace-StringInFile(
+    [string]$input_file,
+    [string]$output_file,
+    [string]$target_string,
+    [string]$replace_string
+)
+{
+    $text = Get-Content $input_file
+    $updated_text = $text -replace $target_string, $replace_string
+    $updated_text | Out-File -FilePath $output_file -Encoding ascii
+}
+
+function Register-Environment(
+    $workspace_config,
+    $component_config,
+    [string]$environment_file
+)
+{
+    $temp_file = "$environment_file.updated"
+
+    Write-Host "Updating version in $environment_file"
+    Replace-StringInFile -input_file $environment_file -output_file $temp_file -target_string 'VERSION_REPLACEMENT_STRING' -replace_string $component_config.version
+    
+    Write-Host "Registering $temp_file"
+    az ml environment create --resource-group $workspace_config.resource_group --workspace $workspace_config.workspace_name --file $temp_file
+}
+
+$ws = Read-JsonConfig('config.json')
+$component_config = Read-JsonConfig('component_config.json')
+$reg_config = Read-JsonConfig($path_to_registration_json)
+
+
+$component_directory = [System.IO.Path]::GetDirectoryName($path_to_registration_json)
+Write-Host "Directory containing components: $component_directory"
+Push-Location $component_directory
+
+foreach ($env_file in $reg_config.environments) {
+    Register-Environment -workspace_config $ws -component_config $component_config -environment_file $env_file
+}
+
+
+Pop-Location
