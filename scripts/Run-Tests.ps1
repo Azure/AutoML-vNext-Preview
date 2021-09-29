a8614b8b250c4054d90c902b618c888f0579635e
Richard Edgar <riedgar@microsoft.com>
2021-09-29 08:28:30 -0400

    Fix test invocation
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Run-Tests.ps1
+++ scripts/Run-Tests.ps1
@@ -1,4 +1,4 @@
 # Enable all the commands
 $Env:AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED=$true
 
-python -m pytest tests --timeout=1200 -o junit_family=xunit2 --junitxml=junit.xml
+python -m pytest ./tests/ -o junit_family=xunit2 --junitxml=junit.xml
