051f82bac034224e2b00705f5a6eb5717ef2ff6c
Richard Edgar <riedgar@microsoft.com>
2021-09-29 12:05:32 -0400

    Make sure end job status is checked
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_rai_pipeline.py
+++ test/rai/test_rai_pipeline.py
@@ -3,9 +3,16 @@
 # ---------------------------------------------------------
 
 import json
+import logging
+import time
+import sys
 
 from azure.ml import MLClient
 from azure.ml.entities import Job
+
+_logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
 
 def get_component_config():
     config_file = 'component_config.json'
@@ -54,3 +61,11 @@ def test_pipeline_from_yaml():
 
     created_job = client.jobs.create_or_update(pipeline_job)
     assert created_job is not None
+
+    while created_job.status not in ['Completed', 'Failed', 'Canceled', 'NotResponding']:
+        time.sleep(30)
+        created_job = client.jobs.get(created_job.name)
+        print("Latest status : {0}".format(created_job.status))
+        _logger.info("Latest status : {0}".format(created_job.status))
+        
+    assert created_job.status == 'Completed'
