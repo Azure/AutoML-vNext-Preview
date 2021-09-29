b88860a0f628c7c4e5426bc7eeef66695b05e059
Richard Edgar <riedgar@microsoft.com>
2021-09-29 12:41:46 -0400

    Move some things to fixtures
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_rai_pipeline.py
+++ test/rai/test_rai_pipeline.py
@@ -5,66 +5,36 @@
 import json
 import logging
 import time
-import sys
 
-from azure.ml import MLClient
 from azure.ml.entities import Job
 
 _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 
-def get_component_config():
-    config_file = 'component_config.json'
-
-    with open(config_file, 'r') as cf:
-        result = json.load(cf)
-
-    return result
-
-def get_workspace_config():
-    ws_config_file = 'config.json'
-
-    with open(ws_config_file) as cf:
-        result = json.load(cf)
-
-    return result
-
-def test_pipeline_from_yaml():
+def test_pipeline_from_yaml(ml_client, component_config):
     pipeline_file = "test/rai/pipeline_analyse.yaml"
     pipeline_processed_file = "pipeline_analyse.processed.yaml"
-
-    component_config = get_component_config()
 
     replacements = {
         'VERSION_REPLACEMENT_STRING': str(component_config['version'])
     }
-    with open(pipeline_file, 'r') as infile, open(pipeline_processed_file, 'w')as outfile:
+    with open(pipeline_file, 'r') as infile, open(pipeline_processed_file, 'w') as outfile:
         for line in infile:
             for f, r in replacements.items():
                 line = line.replace(f, r)
             outfile.write(line)
 
-    # Now get the client
-    ws_config = get_workspace_config()
-
-    client = MLClient(
-        ws_config['subscription_id'],
-        ws_config['resource_group'],
-        ws_config['workspace_name'],
-        logging_enable=True
-    )
-
     pipeline_job = Job.load(
         path=pipeline_processed_file
     )
 
-    created_job = client.jobs.create_or_update(pipeline_job)
+    created_job = ml_client.jobs.create_or_update(pipeline_job)
     assert created_job is not None
 
     while created_job.status not in ['Completed', 'Failed', 'Canceled', 'NotResponding']:
         time.sleep(30)
-        created_job = client.jobs.get(created_job.name)
+        created_job = ml_client.jobs.get(created_job.name)
         print("Latest status : {0}".format(created_job.status))
         _logger.info("Latest status : {0}".format(created_job.status))
         
