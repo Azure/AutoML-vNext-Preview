608a157dbce3882fe74f0bb9ec91aeefae854eb8
Richard Edgar <riedgar@microsoft.com>
2021-10-01 14:34:07 -0400

    Switch to WCUS
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Obtain-Workspace.ps1
+++ scripts/Obtain-Workspace.ps1
@@ -12,7 +12,7 @@
 # component_config.json -> Specifies the version of everything to upload (as epoch_seconds)
 
 $baseName="amlisdkv2"
-$location="EastUS2"
+$location="WestCentralUS"
 $createdTag="createdAt"
 $ownerTeamTagKey="owningTeam"
 $ownerTeamTagValue="AML_Intelligence"
