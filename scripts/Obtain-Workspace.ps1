49055a7c5e4dcc04f6c18ef55294ebb9cba743be
Richard Edgar <riedgar@microsoft.com>
2021-10-01 14:55:19 -0400

    Change regoin and window
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Obtain-Workspace.ps1
+++ scripts/Obtain-Workspace.ps1
@@ -12,14 +12,14 @@
 # component_config.json -> Specifies the version of everything to upload (as epoch_seconds)
 
 $baseName="amlisdkv2"
-$location="WestCentralUS"
+$location="EastUS"
 $createdTag="createdAt"
 $ownerTeamTagKey="owningTeam"
 $ownerTeamTagValue="AML_Intelligence"
 $purposeTagKey="workspacePurpose"
 $purposeTagValue="Automated_Tests_for_DPv2"
 $workspaceYAML="workspace.yaml"
-$window_seconds = 24*36
+$window_seconds = 24*3600
 
 function Get-RecentResourceGroups(
     [int]$min_epoch
