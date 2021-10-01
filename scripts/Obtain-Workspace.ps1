b2c98b40c5af363bb7f8f7a5781128116a393f1c
Richard Edgar <riedgar@microsoft.com>
2021-10-01 15:44:35 -0400

    Try different SDK. Go back to EastUS2 and short workspace window
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Obtain-Workspace.ps1
+++ scripts/Obtain-Workspace.ps1
@@ -12,14 +12,14 @@
 # component_config.json -> Specifies the version of everything to upload (as epoch_seconds)
 
 $baseName="amlisdkv2"
-$location="EastUS"
+$location="EastUS2"
 $createdTag="createdAt"
 $ownerTeamTagKey="owningTeam"
 $ownerTeamTagValue="AML_Intelligence"
 $purposeTagKey="workspacePurpose"
 $purposeTagValue="Automated_Tests_for_DPv2"
 $workspaceYAML="workspace.yaml"
-$window_seconds = 24*3600
+$window_seconds = 24*36
 
 function Get-RecentResourceGroups(
     [int]$min_epoch
