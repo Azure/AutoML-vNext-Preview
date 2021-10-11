0cb9c6d68bd85202673714b593029fac61d534b8
Richard Edgar <riedgar@microsoft.com>
2021-10-11 16:43:03 -0400

    Changing string name in model info file
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/responsibleai/rai_analyse/constants.py
+++ src/responsibleai/rai_analyse/constants.py
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------
 
 class Constants:
-    MODEL_ID_KEY = "model_id"
+    MODEL_ID_KEY = "id"
     MODEL_INFO_FILENAME = "model_info.json"
     MA_RUN_ID_KEY = "model_analysis_run_id"
     MODEL_ANALYSIS_PARENT_FILENAME = "model_analysis.json"
