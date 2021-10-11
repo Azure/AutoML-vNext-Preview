b99ea96c71d63bae6355343406fee1e9492fb4bf
Richard Edgar <riedgar@microsoft.com>
2021-10-11 16:44:45 -0400

    Add comment
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/responsibleai/rai_analyse/constants.py
+++ src/responsibleai/rai_analyse/constants.py
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------
 
 class Constants:
-    MODEL_ID_KEY = "id"
+    MODEL_ID_KEY = "id"  # To match Model schema
     MODEL_INFO_FILENAME = "model_info.json"
     MA_RUN_ID_KEY = "model_analysis_run_id"
     MODEL_ANALYSIS_PARENT_FILENAME = "model_analysis.json"
