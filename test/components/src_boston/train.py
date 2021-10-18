973d6314db5aa8a7e9c38605161639c78a658156
Richard Edgar <riedgar@microsoft.com>
2021-10-18 15:42:06 -0400

    Add RAI Regression Pipeline (#17)
    Add a regression example based on the Boston housing dataset. Undertake some minor refactoring of the test code as part of this.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ test/components/src_boston/train.py
@@ -0,0 +1,119 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+import argparse
+import json
+import os
+import shutil
+import tempfile
+
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+import mlflow
+import mlflow.sklearn
+
+from azureml.core.run import Run
+
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", type=str,
+                        help="Path to training data")
+    parser.add_argument("--target_column_name", type=str,
+                        help="Name of target column")
+    parser.add_argument("--continuous_features", type=json.loads)
+    parser.add_argument("--categorical_features", type=json.loads)
+    parser.add_argument("--model_output", type=str,
+                        help="Path of output model")
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+def get_regression_model_pipeline(continuous_features, categorical_features):
+    # We create the preprocessing pipelines for both numeric and
+    # categorical data.
+    numeric_transformer = Pipeline(steps=[
+        ('scaler', StandardScaler())])
+
+    categorical_transformer = Pipeline(steps=[
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+    transformations = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, continuous_features),
+            ('cat', categorical_transformer, categorical_features)])
+
+    # Append classifier to preprocessing pipeline.
+    # Now we have a full prediction pipeline.
+    pipeline = Pipeline(steps=[('preprocessor', transformations),
+                               ('regressor', RandomForestRegressor())])
+    return pipeline
+
+
+def main(args):
+    current_experiment = Run.get_context().experiment
+    tracking_uri = current_experiment.workspace.get_mlflow_tracking_uri()
+    print("tracking_uri: {0}".format(tracking_uri))
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(current_experiment.name)
+
+    # Read in data
+    print("Reading data")
+    train_dataset = pd.read_parquet(args.training_data)
+
+    # Drop the labeled column to get the training set.
+    y_train = train_dataset[args.target_column_name]
+    X_train = train_dataset.drop(
+        columns=[args.target_column_name])
+
+    continuous_features = args.continuous_features
+    categorical_features = args.categorical_features
+
+    pipeline = get_regression_model_pipeline(
+        continuous_features=continuous_features,
+        categorical_features=categorical_features)
+    model = pipeline.fit(X_train, y_train)
+
+    # Saving model with mlflow
+    with tempfile.TemporaryDirectory() as td:
+        print("Saving model with MLFlow to temporary directory")
+        tmp_output_dir = os.path.join(td, "my_model_dir")
+        mlflow.sklearn.save_model(sk_model=model, path=tmp_output_dir)
+
+        print("Copying MLFlow model to output path")
+        for file_name in os.listdir(tmp_output_dir):
+            print("  Copying: ", file_name)
+            # As of Python 3.8, copytree will acquire dirs_exist_ok as
+            # an option, removing the need for listdir
+            shutil.copy2(src=os.path.join(tmp_output_dir, file_name),
+                         dst=os.path.join(args.model_output, file_name))
+
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
--- test/rai/test_rai_pipeline.py
+++ test/rai/test_rai_pipeline.py
@@ -11,32 +11,56 @@ from azure.ml.entities import Job
 _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
+
+def process_file(input_file, output_file, replacements):
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            for f, r in replacements.items():
+                line = line.replace(f, r)
+            outfile.write(line)
+
+
+def submit_and_wait(ml_client, pipeline_job):
+    created_job = ml_client.jobs.create_or_update(pipeline_job)
+    assert created_job is not None
+
+    while created_job.status not in ['Completed', 'Failed', 'Canceled', 'NotResponding']:
+        time.sleep(30)
+        created_job = ml_client.jobs.get(created_job.name)
+        print("Latest status : {0}".format(created_job.status))
+        _logger.info("Latest status : {0}".format(created_job.status))
+    assert created_job.status == 'Completed'
+
+
 class TestRAI:
-    def test_pipeline_from_yaml(self, ml_client, component_config):
+    def test_classification_pipeline_from_yaml(self, ml_client, component_config):
         current_dir = pathlib.Path(__file__).parent.absolute()
-        pipeline_file = current_dir / "pipeline_analyse.yaml"
-        pipeline_processed_file = "pipeline_analyse.processed.yaml"
+        pipeline_file = current_dir / "pipeline_adult_analyse.yaml"
+        pipeline_processed_file = "pipeline_adult_analyse.processed.yaml"
 
         replacements = {
             'VERSION_REPLACEMENT_STRING': str(component_config['version'])
         }
-        with open(pipeline_file, 'r') as infile, open(pipeline_processed_file, 'w') as outfile:
-            for line in infile:
-                for f, r in replacements.items():
-                    line = line.replace(f, r)
-                outfile.write(line)
+        process_file(pipeline_file, pipeline_processed_file, replacements)
 
         pipeline_job = Job.load(
             path=pipeline_processed_file
         )
 
-        created_job = ml_client.jobs.create_or_update(pipeline_job)
-        assert created_job is not None
+        submit_and_wait(ml_client, pipeline_job)
 
-        while created_job.status not in ['Completed', 'Failed', 'Canceled', 'NotResponding']:
-            time.sleep(30)
-            created_job = ml_client.jobs.get(created_job.name)
-            print("Latest status : {0}".format(created_job.status))
-            _logger.info("Latest status : {0}".format(created_job.status))
-            
-        assert created_job.status == 'Completed'
+    def test_boston_pipeline_from_yaml(self, ml_client, component_config):
+        current_dir = pathlib.Path(__file__).parent.absolute()
+        pipeline_file = current_dir / "pipeline_boston_analyse.yaml"
+        pipeline_processed_file = "pipeline_boston_analyse.processed.yaml"
+
+        replacements = {
+            'VERSION_REPLACEMENT_STRING': str(component_config['version'])
+        }
+        process_file(pipeline_file, pipeline_processed_file, replacements)
+
+        pipeline_job = Job.load(
+            path=pipeline_processed_file
+        )
+
+        submit_and_wait(ml_client, pipeline_job)
