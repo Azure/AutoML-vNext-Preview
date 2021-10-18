ad7c6db9286c4ae28a2a2ab0973e6e66bd12bf48
Richard Edgar <riedgar@microsoft.com>
2021-10-18 10:00:45 -0400

    Starting to assemble things
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

---
+++ northstar/src/simple_script.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+
+if __name__ == "__main__":
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
