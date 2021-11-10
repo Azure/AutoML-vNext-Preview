ff3aa48bd4fe016fd7e0624b0cc075fb77aba774
Richard Edgar <riedgar@microsoft.com>
2021-11-10 09:47:37 -0500

    Build pipeline in Python (#22)
    Add a test case which builds the `PipelineJob` object entirely in Python, rather than reading in a YAML file. Also tweak the scaledown time of the compute cluster.
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- test/rai/test_rai_pipeline.py
+++ test/rai/test_rai_pipeline.py
@@ -6,7 +6,8 @@ import logging
 import pathlib
 import time
 
-from azure.ml.entities import Job
+from azure.ml.entities import InputDatasetEntry
+from azure.ml.entities import ComponentJob, Job, PipelineJob
 
 _logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
@@ -63,4 +64,92 @@ class TestRAI:
             path=pipeline_processed_file
         )
 
+        submit_and_wait(ml_client, pipeline_job)
+
+    def test_classification_pipeline(self, ml_client, component_config):
+        # This only configures an explanation for simplicity
+        version_string = component_config['version']
+
+        # Configure the global pipeline inputs:
+        pipeline_inputs = {
+            'target_column_name': 'income',
+            'my_training_data': InputDatasetEntry(dataset=f"Adult_Train_PQ:{version_string}"),
+            'my_test_data': InputDatasetEntry(dataset=f"Adult_Test_PQ:{version_string}")
+        }
+
+        # Specify the training job
+        train_job_inputs = {
+            'target_column_name': '${{inputs.target_column_name}}',
+            'training_data': '${{inputs.my_training_data}}',
+        }
+        train_job_outputs = {
+            'model_output': None
+        }
+        train_job = ComponentJob(
+            component=f"TrainLogisticRegressionForRAI:{version_string}",
+            inputs=train_job_inputs,
+            outputs=train_job_outputs
+        )
+
+        # The model registration job
+        register_job_inputs = {
+            'model_input_path': '${{jobs.train-model-job.outputs.model_output}}',
+            'model_base_name': 'notebook_registered_logreg',
+        }
+        register_job_outputs = {
+            'model_info_output_path': None
+        }
+        register_job = ComponentJob(
+            component=f"RegisterModel:{version_string}",
+            inputs=register_job_inputs,
+            outputs=register_job_outputs
+        )
+
+        # Top level Model Analysis component
+        create_ma_inputs = {
+            'title': 'Run built from Python',
+            'task_type': 'classification',
+            'model_info_path': '${{jobs.register-model-job.outputs.model_info_output_path}}',
+            'train_dataset': '${{inputs.my_training_data}}',
+            'test_dataset': '${{inputs.my_test_data}}',
+            'target_column_name': '${{inputs.target_column_name}}',
+            'X_column_names': '["Age", "Workclass", "Education-Num", "Marital Status", "Occupation", "Relationship", "Race", "Sex", "Capital Gain", "Capital Loss", "Hours per week", "Country"]',
+            'datastore_name': 'workspaceblobstore',
+            'categorical_column_names': '["Race", "Sex", "Workclass", "Marital Status", "Country", "Occupation"]',
+        }
+        create_ma_outputs = {
+            'model_analysis_info': None
+        }
+        create_ma_job = ComponentJob(
+            component=f"AzureMLModelAnalysis:{version_string}",
+            inputs=create_ma_inputs,
+            outputs=create_ma_outputs
+        )
+
+        # Setup the explanation
+        explain_inputs = {
+            'comment': 'Insert text here',
+            'model_analysis_info': '${{jobs.create-ma-job.outputs.model_analysis_info}}'
+        }
+        explain_job = ComponentJob(
+            component=f"AzureMLModelAnalysisExplanation:{version_string}",
+            inputs = explain_inputs
+        )
+
+        # Assemble into a pipeline
+        pipeline_job = PipelineJob(
+            experiment_name=f"Classification_from_Python_{version_string}",
+            description="Python submitted Adult",
+            jobs = {
+                'train-model-job': train_job,
+                'register-model-job': register_job,
+                'create-ma-job': create_ma_job,
+                'explain-ma-job': explain_job,
+            },
+            inputs=pipeline_inputs,
+            outputs=train_job_outputs,
+            compute="cpucluster"
+        )
+
+        # Send it
         submit_and_wait(ml_client, pipeline_job)
