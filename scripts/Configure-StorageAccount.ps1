40f41d065ea20bc9b2e93c7dfe485546645830e3
Richard Edgar <riedgar@microsoft.com>
2021-11-17 14:30:56 -0500

    Need to install the right module
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- scripts/Configure-StorageAccount.ps1
+++ scripts/Configure-StorageAccount.ps1
@@ -15,7 +15,12 @@ Write-Host "Workspace: $($ws_info.workspace_name)"
 
 $ws = az ml workspace show --resource-group $ws_info.resource_group --name $ws_info.workspace_name | ConvertFrom-Json
 
+Write-Host
 Write-Host "Storage Account: $($ws.storage_account)"
+Write-Host
+
+# Install the command we need
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 
 # Set the permissions
 New-AzRoleAssignment -ObjectID $env:servicePrincipalId `
