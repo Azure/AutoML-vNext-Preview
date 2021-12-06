df50619f29ea9461bd63b4d11f3fb9b58e91371f
Richard Edgar <riedgar@microsoft.com>
2021-12-06 09:55:08 -0500

    Adding function to get the components
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/azure-ml-rai/azure_ml_rai/_list_rai_runs.py
+++ src/azure-ml-rai/azure_ml_rai/_list_rai_runs.py
@@ -10,6 +10,16 @@ from azure.ml import MLClient
 from ._constants import PropertyKeyValues
 
 
+def _get_v1_workspace_client(ml_client: MLClient) -> Workspace:
+    subscription = ml_client._workspace_scope.subscription_id
+    rg_name = ml_client._workspace_scope.resource_group_name
+    ws_name = ml_client._workspace_scope.workspace_name
+
+    v1_workspace = Workspace(subscription, rg_name, ws_name, auth=None)
+
+    return v1_workspace
+
+
 def list_rai_insights(
     ml_client: MLClient,
     experiment_name: str,
@@ -24,15 +34,31 @@ def list_rai_insights(
         filter_properties[PropertyKeyValues.RAI_INSIGHTS_MODEL_ID_KEY] = model_id
 
     # Have to use V1 client for now
-    creds = ml_client._credential
+    v1_workspace = _get_v1_workspace_client(ml_client)
+    v1_experiment = v1_workspace.experiments[experiment_name]
 
-    subscription = ml_client._workspace_scope.subscription_id
-    rg_name = ml_client._workspace_scope.resource_group_name
-    ws_name = ml_client._workspace_scope.workspace_name
-
-    v1_workspace = Workspace(subscription, rg_name, ws_name, auth=None)
-    experiment = v1_workspace.experiments[experiment_name]
-
-    all_runs = Run.list(experiment, properties=filter_properties, include_children=True)
+    all_runs = Run.list(
+        v1_experiment, properties=filter_properties, include_children=True)
 
     return [r.id for r in all_runs]
+
+
+def list_components_for_rai_insight(
+    ml_client: MLClient,
+    experiment_name: str,
+    rai_insight_id: str
+) -> List[str]:
+    v1_workspace = _get_v1_workspace_client(ml_client)
+    v1_experiment = v1_workspace.experiments[experiment_name]
+
+    filter_properties = {
+        PropertyKeyValues.RAI_INSIGHTS_CONSTRUCTOR_RUN_ID_KEY: rai_insight_id
+    }
+
+    all_runs = Run.list(
+        v1_experiment, properties=filter_properties, include_children=True)
+
+    return [
+        (r.id, r.properties[PropertyKeyValues.RAI_INSIGHTS_TYPE_KEY])
+        for r in all_runs
+    ]
