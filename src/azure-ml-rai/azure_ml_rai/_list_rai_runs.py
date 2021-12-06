51c1638dca62e147949aeea9298f75212f187d70
Richard Edgar <riedgar@microsoft.com>
2021-12-06 09:46:44 -0500

    Debugging the list call
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/azure-ml-rai/azure_ml_rai/_list_rai_runs.py
+++ src/azure-ml-rai/azure_ml_rai/_list_rai_runs.py
@@ -24,15 +24,15 @@ def list_rai_insights(
         filter_properties[PropertyKeyValues.RAI_INSIGHTS_MODEL_ID_KEY] = model_id
 
     # Have to use V1 client for now
-    creds = MLClient._credential
+    creds = ml_client._credential
 
-    subscription = MLClient._workspace_scope.subscription_id
-    rg_name = MLClient._workspace_scope.resource_group_name
-    ws_name = MLClient._workspace_score.workspace_name
+    subscription = ml_client._workspace_scope.subscription_id
+    rg_name = ml_client._workspace_scope.resource_group_name
+    ws_name = ml_client._workspace_scope.workspace_name
 
-    v1_workspace = Workspace(subscription, rg_name, ws_name, creds)
+    v1_workspace = Workspace(subscription, rg_name, ws_name, auth=None)
     experiment = v1_workspace.experiments[experiment_name]
 
-    all_runs = Run.list(experiment, properties=filter_properties)
+    all_runs = Run.list(experiment, properties=filter_properties, include_children=True)
 
     return [r.id for r in all_runs]
