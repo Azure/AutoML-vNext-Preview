973e818e8e75cdec6d4a37667edc87679fe5a1af
Richard Edgar <riedgar@microsoft.com>
2021-12-06 10:35:13 -0500

    Starting work on the downloads
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- src/azure-ml-rai/azure_ml_rai/_download_rai_insights.py
+++ src/azure-ml-rai/azure_ml_rai/_download_rai_insights.py
@@ -2,13 +2,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+import json
+import os
+import tempfile
 
 import mlflow
 from mlflow.tracking import MlflowClient
 
 from azure.ml import MLClient
 
+from ._constants import OutputPortNames
 from ._utilities import _get_v1_workspace_client
+
 
 def download_rai_insights(
     ml_client: MLClient,
@@ -21,4 +26,15 @@ def download_rai_insights(
 
     mlflow_client = MlflowClient()
 
-    
+    with tempfile.TemporaryDirectory() as temp_dir:
+        mlflow_client.download_artifacts(
+            rai_insight_id, 
+            OutputPortNames.RAI_INSIGHTS_CONSTRUCTOR_OUTPUT_PORT, 
+            temp_dir)
+
+        json_filename = os.path.join(temp_dir, OutputPortNames.RAI_INSIGHTS_CONSTRUCTOR_OUTPUT_PORT)
+
+        with open(json_filename, 'r') as json_file:
+            constructor_location_info = json.load(json_file)
+
+        print(constructor_location_info)
